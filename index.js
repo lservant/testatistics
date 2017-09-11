02eff9338119f2fda0b1939c5bf3528d12f59679
Louis Servant <louservant@gmail.com>
2017-09-10 22:18:12 -0500

    Tallying system

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+ko.bindingHandlers.enterkey = {
+    init: function (element, valueAccessor, allBindings, viewModel) {
+        var callback = valueAccessor();
+        $(element).keypress(function (event) {
+            var keyCode = (event.which ? event.which : event.keyCode);
+            if (keyCode === 13) {
+                callback.call(viewModel);
+                return false;
+            }
+            return true;
+        });
+    }
+};
+
 var MainViewModel = function(){
     var self = this;
 
@@ -6,11 +20,36 @@ var MainViewModel = function(){
         that.Name = ko.observable("New Hour");
         that.NumberOfStudents = ko.observable(1);
         that.MissedQuestions = ko.observableArray();
+        that.Tallies = ko.observable();
+
+        that.RemoveQuestion = function(question){
+            var removed = that.MissedQuestions.remove(question); // remove all who match the number
+            removed.forEach(function(r){ that.MissedQuestions.unshift(r); }); // put them in front
+            that.MissedQuestions.shift(); // knock one off
+        }
+    }
+
+    function TallyScores(missedQs){
+        var rtn = [];
+        
+        missedQs.forEach(function(q) {
+            if(rtn[q] == undefined){
+                rtn[q] = 1;
+            }else{
+                rtn[q] += 1;
+            }
+        }, this);
+
+        console.log(rtn);
+        return rtn;
     }
 
     self.Questions = ko.observable(10);
     self.ClassPeriods = ko.observableArray();
     self.SelectedClassPeriod = ko.observable();
+    self.SelectedClassPeriod.subscribe(function (classPeriod){
+        classPeriod.Tallies(TallyScores(classPeriod.MissedQuestions()));
+    })
     self.MissedQuestion = ko.observable();
 
     self.NumberOfClassPeriods = ko.pureComputed(function(){
@@ -32,8 +71,8 @@ var MainViewModel = function(){
         self.ClassPeriods.push(newClass);
         self.selectClassPeriod(newClass);
     }
-    self.selectClassPeriod = function (ClassPeriod) {
-        self.SelectedClassPeriod(ClassPeriod);
+    self.selectClassPeriod = function (classPeriod) {
+        self.SelectedClassPeriod(classPeriod);
     }
     self.SubmitMissedQuestion = function(){
         var c = self.SelectedClassPeriod(),
@@ -43,6 +82,12 @@ var MainViewModel = function(){
             self.MissedQuestion(undefined);
         }
     }
+    self.TallyScores = function(){
+        var current = self.SelectedClassPeriod();
+        if(current){
+            current.Tallies(TallyScores(current.MissedQuestions()));
+        }
+    };
 
     self.addClassPeriod();
 }
